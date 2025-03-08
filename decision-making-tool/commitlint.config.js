a523070a6f849cde180a876e3449ea1c3f6d6f98
May <abeillee@bk.ru>
2025-03-08 08:47:48 +0300

    chore: update settings for husky

--- decision-making-tool/commitlint.config.js
+++ decision-making-tool/commitlint.config.js
@@ -1,6 +1,3 @@
-export default {
-    extends: [
-      'lerna', // prefixed with commitlint-config-* 
-      '@commitlint/config-conventional' // scoped packages are not prefixed
-    ]
-  }
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+};
