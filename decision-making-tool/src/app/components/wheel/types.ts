aec7d143add205862e48eba10d5ab971fd98daf5
May <abeillee@bk.ru>
2025-03-17 11:41:49 +0300

    refactor: add types for wheel class

---
+++ decision-making-tool/src/app/components/wheel/types.ts
@@ -0,0 +1,22 @@
+export type OptionsParams = {
+    id: number;
+    title: string;
+    weight: string;
+};
+export type optionNameParams = {
+    startAngle: number;
+    sliceAngle: number;
+    options: OptionsParams;
+    centerX: number;
+    centerY: number;
+};
+export type centerElement = {
+    x: number;
+    y: number;
+    radius: number;
+};
+export enum WheelState {
+    INITIAL = 'initial',
+    PICKING = 'picking',
+    PICKED = 'picked',
+}
